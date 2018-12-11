d8fd29985c0c6eb01d6ddedce485dcf77052057f
ArthurDrc <44248355+ArthurDrc@users.noreply.github.com>
2018-12-11 17:09:08 -0500

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyAr7UbAV_HHbhdiltmeeAhJrCtOo3xTWZw"));
+define('GJ_CODE', getenv("US"));
+define('SITE_NAME',getenv("WeTube"));
+define('TITLENAME', getenv("WeTube"));
+define('EN2DEKEY',getenv("#%$^&*&^&%^#$%^*()HHHJLBHwdjsdk282131372wqhdjsaxmcd"));
+define('EMAIL', getenv("6achen@gmail.com"));
 ?>
