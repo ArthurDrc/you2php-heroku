3b10ea609af66322e759626ddd37547fdb5adc2a
ArthurDrc <44248355+ArthurDrc@users.noreply.github.com>
2018-12-11 17:11:23 -0500

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("AIzaSyAr7UbAV_HHbhdiltmeeAhJrCtOo3xTWZw"));
+define('APIKEY', getenv("Abuu"));
 define('GJ_CODE', getenv("US"));
 define('SITE_NAME',getenv("WeTube"));
 define('TITLENAME', getenv("WeTube"));
 define('EN2DEKEY',getenv("#%$^&*&^&%^#$%^*()HHHJLBHwdjsdk282131372wqhdjsaxmcd"));
-define('EMAIL', getenv("6achen@gmail.com"));
+define('EMAIL', getenv("@gmail.com"));
 ?>
